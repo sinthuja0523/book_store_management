92944c718f4da9392088bfac929d1f3731e56eb9
Mahendran Lavan <mahendranlavan29@gmail.com>
2025-03-12 02:36:11 +0530

    Add project files.

---
+++ BookStoreMgt/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace BookStoreMgt
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            txtUsername = new TextBox();
+            txtPassword = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(169, 301);
+            button1.Name = "button1";
+            button1.Size = new Size(182, 83);
+            button1.TabIndex = 0;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(169, 89);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(100, 23);
+            txtUsername.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(169, 155);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(100, 23);
+            txtPassword.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(598, 539);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsername);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox txtUsername;
+        private TextBox txtPassword;
+    }
+}
---
+++ BookStoreMgt/Form1.cs
@@ -0,0 +1,42 @@
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace BookStoreMgt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            SqlConnection conn = new SqlConnection("Data Source=DESKTOP-DV35AK6;Initial Catalog=Bank_Customer;Integrated Security=True;Encrypt=True;TrustServerCertificate=True");
+            conn.Open();
+            string query = "SELECT role FROM users WHERE user_name=@Username AND password=@Password";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@Username", username);
+            cmd.Parameters.AddWithValue("@Password", password);
+            object result = cmd.ExecuteScalar();
+            if (result != null)
+            {
+                string role = result.ToString();
+                MessageBox.Show($"Login Successful! Role: {role}", "Success");
+
+                // Open the main dashboard
+                //MainForm mainForm = new MainForm(role);
+                this.Hide();
+               // mainForm.Show();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password.", "Login Failed");
+            }
+        }
+    }
+}
